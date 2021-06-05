7a11c6be75165eea5bbbe0f7e0475a343c5f9c6a
Mathew <matymcleod@gmail.com>
2021-06-05 23:21:54 +0000

    importing myObjects from client.js

---
+++ client.js
@@ -0,0 +1,34 @@
+const net = require("net");
+
+// establishes a connection with the game server
+const connect = function () {
+  const conn = net.createConnection({
+    host: "localhost",
+    port: 50541
+  });
+
+  conn.on("data", () => {
+    console.log('Message from server: you dead, you suck!');
+  });
+
+  // interpret incoming data as text
+  conn.setEncoding("utf8");
+  
+  return conn;
+};
+
+console.log("Connecting ...");
+connect();
+
+
+const num = 14;
+const str = 'lalala'
+const funcy = () =>{
+  console.log('FUNCY!')
+};
+  module.exports = {
+    connection: connect,
+    myNumber: num,
+    myString: str,
+    myfunc: funcy,
+  };
