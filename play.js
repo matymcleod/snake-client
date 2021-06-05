223322687e0bfbfb198cb48afc67ccabc3111e88
Mathew <matymcleod@gmail.com>
2021-06-05 23:22:22 +0000

    importing myObjects from client.js

--- play.js
+++ play.js
@@ -1,21 +1,26 @@
 const net = require("net");
+const { myNumber, myString } = require("./client");
+const myObjects = require("./client")
 
 // establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: "localhost",
-    port: 50541
-  });
+// const connect = function () {
+//   const conn = net.createConnection({
+//     host: "localhost",
+//     port: 50541
+//   });
 
-  conn.on("data", () => {
-    console.log('Message from server: you dead, you suck!');
-  });
+//   conn.on("data", () => {
+//     console.log('Message from server: you dead, you suck!');
+//   });
 
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
+//   // interpret incoming data as text
+//   conn.setEncoding("utf8");
   
-  return conn;
-};
+//   return conn;
+// };
 
-console.log("Connecting ...");
-connect();
+// console.log("Connecting ...");
+// connect();
+
+console.log(myNumber);
+console.log(myString);
